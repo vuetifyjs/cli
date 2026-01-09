f49560724c45e5243452baf661f5574b89634421
Andrei Elkin <andreyyolkin@gmail.com>
2026-01-09 13:32:08 +0100

    fix(vuetify, vuetify0): fix uvr integration

--- packages/shared/src/features/router.ts
+++ packages/shared/src/features/router.ts
@@ -137,10 +137,12 @@ function getFileRouterContent (ts: boolean) {
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 })
 
 export default router
